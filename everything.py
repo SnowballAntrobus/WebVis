2190d51c4cccf2971c2c6aaa752ca8b43ea44db3
Dante Gil-Marin <drgilmarin@icloud.com>
2019-05-14 03:00:13 -0500

    some work

--- everything.py
+++ everything.py
@@ -39,30 +39,56 @@ for i in range(stars):
 # Mountain range setup
 points = count_tag("script")
 print("Points:", points)
-# Draw mountain range
-deviation_y = 100
-mean_y = 400
-previous_x = 0
-previous_y = random.gauss(mean_y, deviation_y)
-section_w = 1920 / points
-for i in range(points):
-    w = random.gauss(section_w, section_w * .25)
-    x = previous_x + w
-    y = random.gauss(mean_y, deviation_y)
-    draw.polygon([(previous_x, previous_y), (x, y), (x, 1080), (previous_x, 1080)], (r - 50, g - 50, b - 50))
-    previous_x = x
-    previous_y = y
-draw.polygon([(previous_x, previous_y), (1920, random.gauss(mean_y, deviation_y)), (1920, 1080), (previous_x, 1080)], (r - 50, g - 50, b - 50))
+if points != 0:
+    # Draw mountain range
+    deviation_y = 100
+    mean_y = 400
+    previous_x = 0
+    previous_y = random.gauss(mean_y, deviation_y)
+    section_w = 1920 / points
+    for i in range(points):
+        w = random.gauss(section_w, section_w * .25)
+        x = previous_x + w
+        y = random.gauss(mean_y, deviation_y)
+        draw.polygon([(previous_x, previous_y), (x, y), (x, 1080), (previous_x, 1080)], (r - 50, g - 50, b - 50))
+        previous_x = x
+        previous_y = y
+    draw.polygon([(previous_x, previous_y), (1920, random.gauss(mean_y, deviation_y)), (1920, 1080), (previous_x, 1080)], (r - 50, g - 50, b - 50))
 
 
 # Draw land
 draw.rectangle([0, 720, 1920, 1080], (r + 50, g + 50, b + 50))
+
+d = soup.find('div')
+d = d.find('div')
+d = d.find('div')
+d = d.find('div')
+print(d)
+
+# Draw headings
+def draw_heading(x1, y1, x2, y2):
+    x = random.randint(x1, x2)
+    y = random.randint(y1, y2)
+    w = random.randint()
+
+
+# Draw li, ol, table
+
+# Draw p
+
+# Div Section
+def div_parsing(html):
+    if count_tag("div") > 0:
+        return
+    else:
+        div_parsing(html)
+
 
 # Cleanup
 del draw
 del soup
 
 # Show image
-im.show()
+#im.show()
 # Save image
 im.save('landscape.jpg')
