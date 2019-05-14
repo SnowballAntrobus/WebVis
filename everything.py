c4c57edb81d1ef9fb7df175b14d42f5e852ecbe8
Dante Gil-Marin <drgilmarin@icloud.com>
2019-05-14 00:58:04 -0500

    added stars

--- everything.py
+++ everything.py
@@ -1,12 +1,17 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as BS
 from PIL import Image, ImageDraw, ImageColor
 import sys
 import random
 
 # Soup setup
 with open("index.html") as fp:
-    soup = BeautifulSoup(fp)
-soup = BeautifulSoup("<html>data</html>")
+    soup = BS(fp, 'html.parser')
+
+
+# Tag Counter
+def count_tag(tag_name):
+    return len(soup.find_all(tag_name))
+
 
 # Image setup
 im = Image.open("index.jpg")
@@ -18,9 +23,25 @@ g = random.randint(0, 225)
 b = random.randint(0, 225)
 
 # Draw land
-draw.rectangle([0, 0, 1920, 1080], (r, g, b))
+draw.rectangle([0, 0, 1920, 1080], (r + 50, g + 50, b + 50))
 # Draw sky
-draw.rectangle([0, 0, 1920, 720], (r+50, g+50, b+50))
+draw.rectangle([0, 0, 1920, 720], (r, g, b))
+
+# Stars setup
+stars = count_tag("link")
+print("Stars:", stars)
+# Draw Stars
+for i in range(stars):
+    x = random.randint(0, 1920)
+    y = random.randint(0, 720)
+    w = random.randint(0, 5)
+    draw.rectangle([x, y, x + w, y + w], (225, 225, 225))
+
+
+# Mountain range setup
+points = count_tag("script")
+print("Points:", points)
 
 del draw
 im.show()
+im.save('landscape.jpg')
